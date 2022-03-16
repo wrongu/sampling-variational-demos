1742a8326e579446401d174ca6bb035747d76270
wrongu <richard.d.lange.13@dartmouth.edu>
2022-03-16 15:31:34 -0400

    fixed --> relative path in run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ PROBLEM="$1"
 LAMBDAS=($(python3 -c "import numpy; print(*map(lambda x: f'{x:.3f}', numpy.logspace(0,1,21)))"))
 SAMPLER_ARGS="num_warmup=1000 num_samples=10000"
 ISVI_ARGS="num_warmup=1000 stochastic_kl=1 num_samples=10000 num_kl_samples=50 adapt delta=0.8"
-CMDSTAN="/Users/richard/Research/libraries/cmdstan"
+CMDSTAN="$ROOT/cmdstan"
 REFRESH=500
 CHAINS=(1 2 3 4)
 
@@ -16,10 +16,6 @@ if [ ! -f $ROOT/$PROBLEM/$PROBLEM ]; then
 fi
 
 cd $ROOT/$PROBLEM
-
-if [ -f nuts_1.csv ]; then
-    rm *.csv *stats.txt
-fi
 
 if [ -f data.json ]; then
     DATA_ARG="data file=data.json"
@@ -39,7 +35,7 @@ for c in ${CHAINS[@]}; do
     fi
 done
 
-# Summaryize stats for NUTS across all chains
+# Summarize stats for NUTS across all chains
 $CMDSTAN/bin/stansummary nuts*.csv > nuts_stats.txt
 
 # RUN OURS (ISVI), FOR EACH VALUE OF LAMBDA
@@ -50,6 +46,6 @@ for l in ${LAMBDAS[@]}; do
         fi
     done
 
-    # Summaryize stats for ISVI across all chains
+    # Summarize stats for ISVI across all chains
     $CMDSTAN/bin/stansummary isvi_${l}_*.csv > isvi_${l}_stats.txt
 done
